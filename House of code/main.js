a3ca5ab21aecedab6eea420c4a04cc1ab9c9a008
Cecilie Sari Voss <ceciliesarivoss@Cecilies-iMac.local>
2019-04-16 16:16:05 +0200

    Foreløbig slider

--- House of code/main.js
+++ House of code/main.js
@@ -38,5 +38,27 @@ window.onclick = function(event) {
   }
 }
 
+// dette er js til Note-slider
+
+var carousel = document.querySelector('.carousel');
+var cellCount = 6;
+var selectedIndex = 0;
+
+function rotateCarousel() {
+  var angle = selectedIndex / cellCount * -360;
+  carousel.style.transform = 'translateZ(-182px) rotateY(' + angle + 'deg)';
+}
+
+var prevButton = document.querySelector('.previous-button');
+prevButton.addEventListener( 'click', function() {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener( 'click', function() {
+  selectedIndex++;
+  rotateCarousel();
+});
 
 
