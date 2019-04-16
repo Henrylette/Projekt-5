07bfecd7f242c915c8f5455dbe8f0fc64c71afaa
Neli0013 <47401949+Neli0013@users.noreply.github.com>
2019-04-16 14:18:53 +0200

    tre klik
    tre klik down, tre mere endnu!! jeg har fået kodet note tiltøj knapperne, i noter og lavet et genvejs knap til noter ved dashborad

---
+++ House of code/javascript/notergenvej.js
@@ -0,0 +1,26 @@
+// dette er js til popuppen
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn1");
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
