e5d93cf0de7df797e2c2c2d310f7e6775a2239fc
Henriette <Henr8588@edu.eal.dk>
2019-04-17 09:02:26 +0200

    Tilføjelser af musskema steps

---
+++ House of code/javascript/musskema.js
@@ -0,0 +1,32 @@
+
+
+var currentTab = 0; 
+showTab(currentTab); 
+
+function showTab(n){
+    var x = document.getElementsByClassName("tab"); 
+    x[n].style.display="block"; 
+    
+    if(n==0){
+        document.getElementById("prevBtn").style.display="none"; }
+    else{
+        document.getElementById("prevBtn").style.display="inline";}
+    if(n==(x.length-1)){
+        document.getElementById("nextBtn").innerHTML ="Submit";}
+    else{
+        document.getElementById("nextBtn").innerHTML= "Next"; 
+    }
+    
+    fixStepIndicatoe(n)
+}
+
+function nextPrev(n){
+    var x = document.getElementsByClassName("tab"); 
+   if (n == 1 && !validateForm()) return false;
+   x[currentTab].style.display = "none";
+   currentTab = currentTab + n;
+   if (currentTab >= x.length) {
+    document.getElementById("regForm").submit();
+    return false;}
+  showTab(currentTab);
+}
