08f8097fe0282b294ac48bc722ae184dc3d2ea6e
fbreichmann <47412323+fbreichmann@users.noreply.github.com>
2019-04-19 09:51:17 +0200

    flyttet js fra html til js dokument

--- House of code/javascript/kalender.js
+++ House of code/javascript/kalender.js
@@ -1,21 +1,46 @@
-let slideIndex = 1; 
-showSlides(slideIndex);
-
-// Next/previous controls 
-function plusSlides(n) {
-    showSlides(slideIndex = n);
-}
-// Thumbnail image controls 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    } 
-}
+const months = [
+          {"name": "Januar", "days": 31}, 
+          {"name": "Februar", "days": 28}, 
+          {"name": "Marts", "days": 31}, 
+          {"name": "April", "days": 30}, 
+          {"name": "Maj", "days": 31}, 
+          {"name": "Juni", "days": 30}, 
+          {"name": "Juli", "days": 31}, 
+          {"name": "August", "days": 31}, 
+          {"name": "September", "days": 30}, 
+          {"name": "Oktober", "days": 31}, 
+          {"name": "November", "days": 30}, 
+          {"name": "December", "days": 31}
+        ];
+        const d = new Date();
+        
+        var curMonth = d.getMonth();
+        
+        function fetchCalendar(key) {
+            if(key == "prev") {
+                if(curMonth == 0) {
+                    curMonth = 11;
+                } else {
+                    curMonth = (curMonth-1);
+                }
+            } else if(key == "next") {
+                if(curMonth == 11) {
+                    curMonth = 0;
+                } else {
+                    curMonth = (curMonth+1);
+                }
+            } else {
+                curMonth = d.getMonth();
+            }
+            updateCalendar(months[curMonth]["name"], months[curMonth]["days"]);
+        }
+        
+        function updateCalendar(month, days) {
+            document.getElementById("monthName").innerHTML = month;
+            document.getElementById("monthDays").innerHTML = "";
+            for(var i=0; i < days; i++){
+                document.getElementById("monthDays").innerHTML += '<div class="dage"> <a>' + (i+1) + '</a> </div>';
+            }
+        }
+        
+window.load = fetchCalendar();
