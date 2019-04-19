3a21650ff98f1008c81fe2214ef1ac1c6f318a31
Henriette <Henr8588@edu.eal.dk>
2019-04-19 21:01:43 +0200

    Tilføjelse af js gem - grundkode

--- House of code/javascript/musskema.js
+++ House of code/javascript/musskema.js
@@ -1,6 +1,6 @@
 
 
-/* Funktion som viser en del af skemaet*/
+//Funktion som viser en del af skemaet
 var currentSkema = 0; 
 showTab(currentSkema); 
 
@@ -8,9 +8,11 @@ function showTab(n){
     
     var x = document.getElementsByClassName("mus-skema-box"); 
     x[n].style.display = "table"; 
+    
+
 }
 
-/* Funktion som viser hvert step i skemaet*/
+// Funktion som viser hvert step i skemaet ved brug af knapperne
 function next(n){
 
 var x = document.getElementsByClassName("mus-skema-box"); 
@@ -23,6 +25,16 @@ var x = document.getElementsByClassName("mus-skema-box");
 }
 
 
+//Funktion som viser den skrevende tekst når brugeren trykker på preview 
+
+function showPreview() {
+    
+    var outputSkema1 = document.getElementById("input1").value; 
+    var outputSkema2 = document.getElementById("input2").value; 
+    
+    document.getElementById("output1").innerText = outputSkema1; 
+    document.getElementById("output2").innerText = outputSkema2;
+}
 
 
 
