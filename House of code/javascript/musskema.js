e62d8ae25b07ffa6b1e497091b155313395e61fd
Henriette <Henr8588@edu.eal.dk>
2019-04-20 00:35:49 +0200

    Gem funktion tilføjelser

--- House of code/javascript/musskema.js
+++ House of code/javascript/musskema.js
@@ -29,11 +29,35 @@ var x = document.getElementsByClassName("mus-skema-box");
 
 function showPreview() {
     
+    //skema - Højt drive
     var outputSkema1 = document.getElementById("input1").value; 
-    var outputSkema2 = document.getElementById("input2").value; 
+    var outputSkema2 = document.getElementById("input2").value;
+    var outputSkema3 = document.getElementById("input3").value; 
+    var outputSkema4 = document.getElementById("input4").value; 
+    var outputSkema5 = document.getElementById("input5").value; 
     
+    //skema - et stærkt team
+    var outputSkema1 = document.getElementById("input6").value; 
+    var outputSkema2 = document.getElementById("input7").value;
+    var outputSkema3 = document.getElementById("input8").value; 
+    var outputSkema4 = document.getElementById("input9").value; 
+    var outputSkema5 = document.getElementById("input10").value; 
+    
+    
+
+    //skema - Højt drive
     document.getElementById("output1").innerText = outputSkema1; 
     document.getElementById("output2").innerText = outputSkema2;
+    document.getElementById("output3").innerText = outputSkema3;
+    document.getElementById("output4").innerText = outputSkema4;
+    document.getElementById("output5").innerText = outputSkema5;
+    
+    //skema - Højt drive
+    document.getElementById("output6").innerText = outputSkema6; 
+    document.getElementById("output7").innerText = outputSkema7;
+    document.getElementById("output8").innerText = outputSkema8;
+    document.getElementById("output9").innerText = outputSkema9;
+    document.getElementById("output10").innerText = outputSkema10;
 }
 
 
