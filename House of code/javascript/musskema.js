11068a751d074cc807eed60a948793b213cda86f
Henriette <Henr8588@edu.eal.dk>
2019-04-19 16:15:39 +0200

    Tilføjet JS til mus-skema
    Man kan nu se musskemaet trin for trin.

--- House of code/javascript/musskema.js
+++ House of code/javascript/musskema.js
@@ -1,32 +1,31 @@
 
 
-var currentTab = 0; 
-showTab(currentTab); 
+/* Funktion som viser en del af skemaet*/
+var currentSkema = 0; 
+showTab(currentSkema); 
 
 function showTab(n){
-    var x = document.getElementsByClassName("tab"); 
-    x[n].style.display="block"; 
     
-    if(n==0){
-        document.getElementById("prevBtn").style.display="none"; }
-    else{
-        document.getElementById("prevBtn").style.display="inline";}
-    if(n==(x.length-1)){
-        document.getElementById("nextBtn").innerHTML ="Submit";}
-    else{
-        document.getElementById("nextBtn").innerHTML= "Next"; 
-    }
-    
-    fixStepIndicatoe(n)
+    var x = document.getElementsByClassName("mus-skema-box"); 
+    x[n].style.display = "table"; 
 }
 
-function nextPrev(n){
-    var x = document.getElementsByClassName("tab"); 
-   if (n == 1 && !validateForm()) return false;
-   x[currentTab].style.display = "none";
-   currentTab = currentTab + n;
-   if (currentTab >= x.length) {
-    document.getElementById("regForm").submit();
-    return false;}
-  showTab(currentTab);
+/* Funktion som viser hvert step i skemaet*/
+function next(n){
+
+var x = document.getElementsByClassName("mus-skema-box"); 
+ 
+  x[currentSkema].style.display = "none";
+
+  currentSkema = currentSkema + n;
+    
+ showTab(currentSkema); 
 }
+
+
+
+
+
+
+
+
