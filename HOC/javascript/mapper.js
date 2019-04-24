08ee077b11dd5a2a47b5e003a1750aea0a66d1b0
Henriette <Henr8588@edu.eal.dk>
2019-04-24 09:22:58 +0200

    Rette stave fejl og fejl ved fold ud

--- HOC/javascript/mapper.js
+++ HOC/javascript/mapper.js
@@ -2,19 +2,21 @@
 //åbner mapper på MUS-side
 function openMapperskema() {
   var x = document.getElementById("mapper-skema");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+    
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
 
 //åbner mapper på Note-side
 function openMappernote() {
   var x = document.getElementById("mapper-noter");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+    
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
